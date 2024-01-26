bd86e180d61b7f2128091997894fd22096531e73
Koushik Romel <koushikromel@gmail.com>
2024-01-26 21:51:18 +0530

    Delete backend directory

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routes import todo as TodoRouter
-from routes import todo as UserRouter
-
-
-app = FastAPI()
-
-app.include_router(TodoRouter)
-app.include_router(UserRouter)
